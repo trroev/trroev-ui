133c6b5f71cc24be223d1ee394eb8eded8069529
Trevor Mathiak <tmathiak@gmail.com>
2023-12-22 15:02:28 -0500

    build: add storybook and update main.ts and preview.ts

---
+++ .storybook/main.ts
@@ -0,0 +1,29 @@
+import path from 'path'
+import type { StorybookConfig } from '@storybook/nextjs'
+
+const config: StorybookConfig = {
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-onboarding',
+    '@storybook/addon-interactions',
+  ],
+  framework: {
+    name: '@storybook/nextjs',
+    options: {},
+  },
+  docs: {
+    autodocs: 'tag',
+  },
+  webpackFinal: async (config) => {
+    config.resolve = config.resolve ?? {}
+    config.resolve.alias = config.resolve.alias ?? {}
+    // Alias '@' to point to the 'src' directory
+    config.resolve.alias['@'] = path.resolve(__dirname, '../src/')
+
+    // Important: return the modified config
+    return config
+  },
+}
+export default config
