9384147ab2c2a5ea1d8dab185af1622e66e975b9
Trevor Mathiak <tmathiak@gmail.com>
2023-12-22 15:38:34 -0500

    style: add base colors and test button component

--- src/components/ui/button/button.stories.ts
+++ src/components/ui/button/button.stories.ts
@@ -3,23 +3,26 @@ import type { Meta, StoryObj } from '@storybook/react'
 
 import { Button, ButtonProps } from './button'
 
+const buttonVariants = [
+  'default',
+  'destructive',
+  'outline',
+  'secondary',
+  'ghost',
+  'link',
+]
+const buttonSizes = ['default', 'sm', 'lg', 'icon']
+
 const argTypes = {
   variant: {
     control: {
       type: 'select',
-      options: [
-        'default',
-        'destructive',
-        'outline',
-        'secondary',
-        'ghost',
-        'link',
-      ],
+      options: buttonVariants,
     },
     description: 'The variant of the button.',
   },
   size: {
-    control: { type: 'select', options: ['default', 'sm', 'lg', 'icon'] },
+    control: { type: 'select', options: buttonSizes },
     description: 'The size of the button.',
   },
   disabled: {
@@ -35,6 +38,11 @@ const meta: Meta<ButtonProps> = {
     layout: 'centered',
   },
   tags: ['autodocs'],
+  args: {
+    onClick: action('onClick'),
+    size: 'default',
+    disabled: false,
+  },
   argTypes,
 }
 
@@ -46,7 +54,36 @@ export const Default: Story = {
   args: {
     children: 'Default Button',
     variant: 'default',
-    size: 'default',
-    disabled: false,
+  },
+}
+
+export const Destructive: Story = {
+  args: {
+    children: 'Destructive Button',
+    variant: 'destructive',
+  },
+}
+export const Ghost: Story = {
+  args: {
+    children: 'Ghost Button',
+    variant: 'ghost',
+  },
+}
+export const Link: Story = {
+  args: {
+    children: 'Link Button',
+    variant: 'link',
+  },
+}
+export const Outline: Story = {
+  args: {
+    children: 'Outline Button',
+    variant: 'outline',
+  },
+}
+export const Secondary: Story = {
+  args: {
+    children: 'Secondary Button',
+    variant: 'secondary',
   },
 }
--- src/components/ui/button/button.tsx
+++ src/components/ui/button/button.tsx
@@ -9,15 +9,15 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: 'bg-blue-600 text-primary-foreground hover:bg-blue-600/90',
+        default: 'bg-primary text-primary-foreground hover:bg-primary/90',
         destructive:
           'bg-destructive text-destructive-foreground hover:bg-destructive/90',
+        ghost: 'hover:bg-accent hover:text-accent-foreground',
+        link: 'text-primary underline-offset-4 hover:underline',
         outline:
           'border border-input bg-background hover:bg-accent hover:text-accent-foreground',
         secondary:
           'bg-secondary text-secondary-foreground hover:bg-secondary/80',
-        ghost: 'hover:bg-accent hover:text-accent-foreground',
-        link: 'text-primary underline-offset-4 hover:underline',
       },
       size: {
         default: 'h-10 px-4 py-2',
