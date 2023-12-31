1d7c55f1a5c23cf9f6623d6554a4de6f769bb4ae
Trevor Mathiak <tmathiak@gmail.com>
2023-12-31 07:13:45 -0500

    feat: add card story

---
+++ src/components/ui/card/card.stories.tsx
@@ -0,0 +1,66 @@
+import { Meta } from '@storybook/react'
+
+import { Card, CardContent, CardProps } from '.'
+
+const argTypes = {
+  variant: {
+    control: {
+      type: 'select',
+      options: ['default'],
+    },
+    description: 'The variant of the card.',
+  },
+  isBlurred: {
+    control: 'boolean',
+    description: 'If true, the card background will be blurred.',
+  },
+  radius: {
+    control: {
+      type: 'select',
+      options: ['none', 'sm', 'md', 'lg'],
+    },
+    description: 'The radius of the card.',
+  },
+  shadow: {
+    control: {
+      type: 'select',
+      options: ['none', 'sm', 'md', 'lg'],
+    },
+    description: 'The shadow of the card.',
+  },
+}
+
+export default {
+  title: 'UI/Card',
+  component: Card,
+  tags: ['autodocs'],
+  argTypes,
+  decorators: [
+    (Story) => (
+      <div className="flex h-full w-full items-center justify-center">
+        <Story />
+      </div>
+    ),
+  ],
+} as Meta<typeof Card>
+
+const defaultProps: CardProps = {
+  variant: 'default',
+  isBlurred: false,
+  radius: 'lg',
+  shadow: 'sm',
+}
+
+const Template = (args: CardProps) => (
+  <Card {...defaultProps} {...args} className="max-w-md">
+    <CardContent className="p-3">
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed</p>
+    </CardContent>
+  </Card>
+)
+
+export const Default = {
+  render: Template,
+
+  args: { ...defaultProps },
+}
