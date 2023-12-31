874174c81e0ff39f3d73e684f83bbd9202d0d204
Trevor Mathiak <tmathiak@gmail.com>
2023-12-31 08:24:05 -0500

    refactor: checkbox story

--- src/components/ui/card/card.stories.tsx
+++ src/components/ui/card/card.stories.tsx
@@ -37,7 +37,7 @@ export default {
   argTypes,
   decorators: [
     (Story) => (
-      <div className="flex h-full w-full items-center justify-center">
+      <div className="flex h-screen w-full items-center justify-center">
         <Story />
       </div>
     ),
--- src/components/ui/checkbox/checkbox.stories.tsx
+++ src/components/ui/checkbox/checkbox.stories.tsx
@@ -1,40 +1,47 @@
-import { Meta, StoryObj } from '@storybook/react'
+import { CheckboxProps } from '@radix-ui/react-checkbox'
+import { Meta } from '@storybook/react'
 
 import { Checkbox } from '.'
 import { Label } from '../label'
 
 const argTypes = {
-  checked: {
-    control: 'boolean',
-    description: 'If true, the checkbox will be checked.',
-  },
   disabled: {
     control: 'boolean',
     description: 'If true, the checkbox will be disabled.',
   },
 }
 
-const meta: Meta = {
+export default {
   title: 'UI/Checkbox',
   component: Checkbox,
-  parameters: {
-    layout: 'centered',
-  },
   tags: ['autodocs'],
   argTypes,
+  decorators: [
+    (Story) => (
+      <div className="flex h-screen w-full items-center justify-center">
+        <Story />
+      </div>
+    ),
+  ],
+} as Meta<typeof Checkbox>
+
+const defaultProps: CheckboxProps = {
+  disabled: false,
 }
 
-export default meta
-
-type Story = StoryObj<typeof Checkbox>
-
-export const Default: Story = {}
-
-export const WithLabel: Story = {
-  render: (args) => (
-    <div className="flex items-center space-x-2">
-      <Checkbox id="terms" {...args} />
-      <Label htmlFor="terms">Accept Terms and Conditions</Label>
-    </div>
-  ),
+const Template = (args: CheckboxProps) => {
+  return <Checkbox {...defaultProps} {...args} aria-label="Default Checkbox" />
 }
+
+export const Default = {
+  render: Template,
+
+  args: { ...defaultProps },
+}
+
+export const WithLabel = () => (
+  <div className="flex items-center space-x-2">
+    <Checkbox id="terms" {...defaultProps} aria-label="Terms Checkbox" />
+    <Label htmlFor="terms">Accept Terms and Conditions</Label>
+  </div>
+)
