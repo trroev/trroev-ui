94054af1944d65728cd60bf191a8e4fbcea24fdd
Trevor Mathiak <tmathiak@gmail.com>
2024-01-02 05:57:11 -0500

    feat: add accordion storybook stories

---
+++ src/components/ui/accordion/accordion.stories.tsx
@@ -0,0 +1,49 @@
+import { action } from '@storybook/addon-actions'
+import { Meta } from '@storybook/react'
+
+import { Accordion, AccordionItem, AccordionProps } from '.'
+
+export default {
+  title: 'UI/Accordion',
+  component: Accordion,
+  tags: ['autodocs'],
+  argTypes: {},
+  decorators: [
+    (Story) => (
+      <div className="flex h-full w-full items-center justify-center">
+        <Story />
+      </div>
+    ),
+  ],
+} as Meta<typeof Accordion>
+
+const defaultProps: AccordionProps = {
+  onClick: action('onClick'),
+}
+
+const Template = (args: AccordionProps) => {
+  const accordionContent =
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis possimus odio sequi hic maiores, unde saepe tempora laborum quam iusto totam, expedita beatae! Odio, laboriosam?'
+
+  return (
+    <div className="w-[350px]">
+      <Accordion className="w-full">
+        <AccordionItem title="Accordion Item 1">
+          {accordionContent}
+        </AccordionItem>
+        <AccordionItem title="Accordion Item 2">
+          {accordionContent}
+        </AccordionItem>
+        <AccordionItem title="Accordion Item 3">
+          {accordionContent}
+        </AccordionItem>
+      </Accordion>
+    </div>
+  )
+}
+
+export const Default = {
+  render: Template,
+
+  args: { ...defaultProps },
+}
