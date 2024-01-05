1fdc1b51419cc8c78969a1709cbd522e22163902
Trevor Mathiak <trevor.mathiak@fusionary.com>
2024-01-05 13:49:51 -0500

    fix: eslint error

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,4 @@
-const { type } = require("os");
+const { type } = require('os')
 
 module.exports = {
   root: true,
@@ -10,70 +10,80 @@ module.exports = {
   ignorePatterns: ['node_modules/*'],
   extends: ['eslint:recommended'],
   overrides: [
-    files: ['**/*.ts', '**/*.tsx'],
-    parser: '@typescript-eslint/parser',
-    settings: {
-      react: { version: 'detect' },
-      'import/resolver': {
-        typescript: {},
+    {
+      files: ['**/*.ts', '**/*.tsx'],
+      parser: '@typescript-eslint/parser',
+      settings: {
+        react: { version: 'detect' },
+        'import/resolver': {
+          typescript: {},
+        },
       },
-    },
-    env: {
-      browser: true,
-      node: true,
-      es6: true,
-    },
-    extends: [
-    'eslint:recommended',
-    'plugin:import/errors',
-    'plugin:import/warnings',
-    'plugin:import/typescript',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
-    'plugin:jsx-a11y/recommended',
-    'plugin:prettier/recommended',
-    'plugin:@shopify/esnext',
-    'plugin:@shopify/react',
-    'plugin:@shopify/prettier',
-    'plugin:tailwindcss/recommended',
-    'next/core-web-vitals',
-    'plugin:storybook/recommended',
-    ],
-    rules: {
-      'no-restricted-imports': [
-        'error',
-        {
-          patterns: ['@/features/*/*'],
-        },
+      env: {
+        browser: true,
+        node: true,
+        es6: true,
+      },
+      extends: [
+        'eslint:recommended',
+        'plugin:import/errors',
+        'plugin:import/warnings',
+        'plugin:import/typescript',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:jsx-a11y/recommended',
+        'plugin:prettier/recommended',
+        'plugin:@shopify/esnext',
+        'plugin:@shopify/react',
+        'plugin:@shopify/prettier',
+        'plugin:tailwindcss/recommended',
+        'next/core-web-vitals',
+        'plugin:storybook/recommended',
       ],
-      'linebreak-style': ['error', 'unix'],
-      'react/prop-types': 'off',
+      rules: {
+        'no-restricted-imports': [
+          'error',
+          {
+            patterns: ['@/features/*/*'],
+          },
+        ],
+        'linebreak-style': ['error', 'unix'],
+        'react/prop-types': 'off',
 
-      'import/order': [
-        'error',
-        {
-          groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index', 'object'],
-          'newlines-between': 'always',
-          alphabetize: { order: 'asc', caseInsensitive: true },
-        },
-      ],
-      'import/default': 'off',
-      'import/no-named-as-default-member': 'off',
-      'import/no-named-as-default': 'off',
+        'import/order': [
+          'error',
+          {
+            groups: [
+              'builtin',
+              'external',
+              'internal',
+              'parent',
+              'sibling',
+              'index',
+              'object',
+            ],
+            'newlines-between': 'always',
+            alphabetize: { order: 'asc', caseInsensitive: true },
+          },
+        ],
+        'import/default': 'off',
+        'import/no-named-as-default-member': 'off',
+        'import/no-named-as-default': 'off',
 
-      'react/react-in-jsx-scope': 'off',
+        'react/react-in-jsx-scope': 'off',
 
-      'jsx-a11y/anchor-is-valid': 'off',
+        'jsx-a11y/anchor-is-valid': 'off',
 
-      '@typescript-eslint/no-unused-vars': ['error'],
+        '@typescript-eslint/no-unused-vars': ['error'],
 
-      '@typescript-eslint/explicit-function-return-type': ['off'],
-      '@typescript-eslint/explicit-module-boundary-types': ['off'],
-      '@typescript-eslint/no-empty-interface': ['off'],
-      '@typescript-eslint/no-explicit-any': ['off'],
+        '@typescript-eslint/explicit-function-return-type': ['off'],
+        '@typescript-eslint/explicit-module-boundary-types': ['off'],
+        '@typescript-eslint/no-empty-interface': ['off'],
+        '@typescript-eslint/no-explicit-any': ['off'],
 
-      'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+        'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+      },
     },
   ],
 }
