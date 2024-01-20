8af6f60d160170645e7baf3cecc4de3579a15558
Trevor Mathiak <tmathiak@gmail.com>
2024-01-19 21:06:20 -0500

    feat: create various background gradients

---
+++ src/styles/gradients.ts
@@ -0,0 +1,47 @@
+export const gradientsWinter = [
+  'bg-gradient-to-tr from-blue-200 to-cyan-600',
+  'bg-gradient-to-r from-sky-200 to-blue-600',
+  'bg-gradient-to-br from-indigo-200 to-cyan-800',
+  'bg-gradient-to-tr from-blue-200 to-indigo-600',
+  'bg-gradient-to-r from-sky-200 to-indigo-600',
+  'bg-gradient-to-br from-indigo-200 to-indigo-600',
+  'bg-gradient-to-br from-indigo-400 via-blue-500 to-sky-500',
+  'bg-gradient-to-tr from-sky-400 via-blue-500 to-indigo-500',
+  'bg-gradient-to-r from-blue-400 via-sky-500 to-indigo-500',
+]
+
+export const gradientsSpring = [
+  'bg-gradient-to-tr from-lime-400 to-green-600',
+  'bg-gradient-to-r from-green-200 to-teal-600',
+  'bg-gradient-to-br from-cyan-400 to-green-600',
+  'bg-gradient-to-tr from-emerald-200 to-sky-600',
+  'bg-gradient-to-r from-cyan-200 to-teal-600',
+  'bg-gradient-to-br from-green-200 to-teal-600',
+  'bg-gradient-to-br from-green-400 via-cyan-500 to-teal-500',
+  'bg-gradient-to-tr from-teal-400 via-cyan-500 to-green-500',
+  'bg-gradient-to-r from-cyan-400 via-teal-500 to-green-500',
+]
+
+export const gradientsSummer = [
+  'bg-gradient-to-tr from-yellow-200 to-purple-600',
+  'bg-gradient-to-r from-pink-200 to-orange-600',
+  'bg-gradient-to-br from-rose-200 to-violet-600',
+  'bg-gradient-to-tr from-yellow-200 to-fuchsia-600',
+  'bg-gradient-to-r from-pink-200 to-purple-600',
+  'bg-gradient-to-br from-rose-200 to-fuchsia-600',
+  'bg-gradient-to-br from-rose-400 via-fuchsia-500 to-purple-500',
+  'bg-gradient-to-tr from-fuchsia-400 via-rose-500 to-pink-500',
+  'bg-gradient-to-r from-rose-400 via-fuchsia-500 to-pink-500',
+]
+
+export const gradientsAutumn = [
+  'bg-gradient-to-tr from-orange-200 to-amber-600',
+  'bg-gradient-to-r from-orange-200 to-amber-200',
+  'bg-gradient-to-br from-yellow-200 to-orange-600',
+  'bg-gradient-to-tr from-yellow-200 to-amber-600',
+  'bg-gradient-to-r from-amber-200 to-green-600',
+  'bg-gradient-to-br from-green-700 to-orange-600',
+  'bg-gradient-to-br from-orange-400 via-amber-500 to-yellow-500',
+  'bg-gradient-to-tr from-yellow-400 via-orange-500 to-amber-500',
+  'bg-gradient-to-r from-amber-400 via-green-500 to-orange-500',
+]
